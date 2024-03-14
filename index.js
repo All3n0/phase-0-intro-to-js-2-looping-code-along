5463c92bfa8472bbc16993ee0b2dbf63f6e95a45
ALLAN <allankipr6@gmail.com>
2024-03-14 20:08:35 +0300

    new loop

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+const names =["Kimbers","Samsons","Shelbys"]
+    const eventName =["Birthday","Easter","winter"]
+function writeCards(names, eventName) {
+    let messages = []; 
+    
+    for (let i = 0; i < names.length; i++) {
+        
+        let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+       
+        messages.push(message);
+    }
+    
+
+    
+    return messages;
+}
+function countDown(number){
+    while(number >=0){
+        console.log(number);
+        number--;
+    }
+    }
